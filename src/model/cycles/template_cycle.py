683c8ad87188306421a89c10e98e023adb3531b3
Takauã Silva <takauan9@gmail.com>
2025-12-17 12:34:51 -0300

    Alteração e implementação de novos arquivos

---
+++ src/model/cycles/template_cycle.py
@@ -0,0 +1,24 @@
+"""
+class TemplateCycle:
+    def __init__(self):
+        pass
+    
+    def save_code(self):
+        pass
+    
+    # Método responsável pelo o arquivo CMP_MAIN.MPF
+    def file_main(self):
+        return self.main
+    
+    # Método responsável pelo o arquivo CMP_INIT.SPF
+    def file_init(self):
+        return self.init
+        
+    # Método responsável pelo o arquivo CMP_CONFIGS.SPF
+    def file_configs(self):
+        return self.configs
+        
+    # Método responsável pelo o arquivo CMP_CYCLE.SPF
+    def file_cycle(self):
+        return self.cycle
+"""
